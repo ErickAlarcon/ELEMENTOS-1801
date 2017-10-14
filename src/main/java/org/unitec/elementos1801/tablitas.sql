162bd35d30e9680ebaa68fdc7a4d047f1521579b
T-107 <T-107@PC206>
2017-10-13 21:47:43 -0500

    segundo

---
+++ src/main/java/org/unitec/elementos1801/tablitas.sql
@@ -0,0 +1,5 @@
+create table Mensajito (id integer primary key auto_inrement, 
+                        titulo varchar (50),
+                        cuerpo varchar (250) );
+
+select * from mensajito;
